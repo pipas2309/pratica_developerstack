aa45b400ba99726cdb2ab38fcb070b3c9fdadb35
Lucas Palharini <lucas230991@hotmail.com>
2022-09-08 20:04:26 -0300

    feat: POST/question created

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
+import { prisma } from '../config/database';
 
 export async function createQuestion(req: Request, res: Response) {
-  // TODO
+  const { askedBy, question } = req.body;
+
+  await prisma.questions.create({data: {askedBy, question}});
+
+  res.sendStatus(201);
 }
 
 export async function createAnswer(req: Request, res: Response) {
